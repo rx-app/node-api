8a46a573baa607cdc07525064f99fda274f8215f
dingming <dingming@hz-health.cn>
2022-03-28 18:55:31 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
-const { APP_PORT } = require('./config/config.default')
+const Koa = require('koa')
 
-const app = require('./app')
+const app = new Koa()
 
-app.listen(APP_PORT, () => {
-  console.log(`server is running on http://localhost:${APP_PORT}`)
+app.use( (ctx,next)=>{
+  ctx.body = 'hellow word'
+} )
+
+app.listen(3000,()=>{
+  console.log( 'server is running at http://localhost:3000/');
 })
