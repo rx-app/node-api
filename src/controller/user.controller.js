7658bb24c944a45ef2c177c2d4507a43fb06a531
dingming <dingming@hz-health.cn>
2022-03-30 19:26:05 +0800

    save

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -9,7 +9,14 @@ class UserController {
         let res = await createUser(user_name,password)
         console.log(res);
         // 3.返回结果
-        ctx.body = '用户注册成功'
+        ctx.body = {
+            code:0,
+            message:'用户注册成功',
+            result:{
+                id:res.id,
+                user_name:res.user_name
+            }
+        }
     }
     async login(ctx,next){
         ctx.body = '登录成功'
