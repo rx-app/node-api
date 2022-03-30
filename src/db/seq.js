fb5eaeb17f0fb1e095f76d0b8864c261c77b7594
dingming <dingming@hz-health.cn>
2022-03-30 16:08:36 +0800

    save

--- src/db/seq.js
+++ src/db/seq.js
@@ -14,9 +14,9 @@ const seq = new Sequelize(MYSQL_DB, MYSQL_USER, MYSQL_PWD, {
     dialect: 'mysql',
   })
 
-seq.authenticate().then( ()=>{
-    console.log('数据库连接成功');
-} ).catch(err=>console.log(err))
+// seq.authenticate().then( ()=>{
+//     console.log('数据库连接成功');
+// } ).catch(err=>console.log(err))
 
 
 module.exports = seq
---
+++ src/model/user.model.js
@@ -0,0 +1,28 @@
+const {DataTypes} = require('sequelize')
+
+const seq = require('../db/seq')
+
+const User = seq.define('zd_user',{
+    user_name:{
+        type:DataTypes.STRING,
+        allowNull:false,
+        unique:true,
+        commnet:'用户名，唯一'
+    },
+    password: {
+        type: DataTypes.CHAR(64),
+        allowNull: false,
+        comment: '密码',
+    },
+    is_admin: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: 0,
+        comment: '是否为管理员, 0: 不是管理员(默认); 1: 是管理员',
+    },
+})
+
+// 强制同步数据库(创建数据表)  
+// User.sync({ force: true })
+
+module.exports = User
